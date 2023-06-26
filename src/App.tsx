90221495c11bc7f4008d076f0b3b591db8a25869
muyiwa johnson <75730906+codewithjohnson@users.noreply.github.com>
2023-06-26 10:31:37 +0100

    removed group transition

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,11 @@
-import { useOutlet } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 import Navbar from "./components/Navbar";
-import { useLocation } from "react-router-dom";
-import { CSSTransition, SwitchTransition } from "react-transition-group";
-import { useRef } from "react";
 
 const App = () => {
-  const location = useLocation();
-  const nodeRef = useRef(null);
-  const currentOutlet = useOutlet();
-
   return (
     <div className="relative">
       <Navbar />
-
-      <SwitchTransition>
-        <CSSTransition
-          in={true}
-          timeout={300}
-          nodeRef={nodeRef}
-          classNames="page"
-          unmountOnExit
-          key={location.pathname}
-        >
-          <div ref={nodeRef} className="page">
-            {currentOutlet}
-          </div>
-        </CSSTransition>
-      </SwitchTransition>
+      <Outlet />
     </div>
   );
 };
